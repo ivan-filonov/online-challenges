b02df0c9b347c0e748b5aa2e3865c577b0b86789
Ivan Filonov <ivan.filonov@gmail.com>
2015-01-27 19:36:09 +0300

    wip

--- grid-walk.py
+++ grid-walk.py
@@ -1,19 +1,27 @@
 def test(x,y):
-	q=0
-	for z in (x,y):
-		z=abs(z)
-		while z > 0:
-			q,z=q+z%10,int(z/10)
-	return q <= 19
-points, ps=set(), [(0,0)]
-def fill(x,y):
-	global points,ps
-	if (x*10000+y) in points:
-		return
-	if not test(x,y):
-		return
-	points.add(x*10000+y)
-	ps = ps + [(x+d*s,y+d*(1-s)) for d in (1,-1) for s in (0,1)]
-while len(ps)>0:
-    fill(*ps.pop())
-print(len(points))
+	return ds(x)+ds(y)
+def ds(n):
+	n,s = abs(n),0
+	while n > 0:
+		n,s = int(n/10), s+n%10
+	return s
+def test(x,y):
+	return ds(x)+ds(y) <= 19
+def idx(x,y):
+	return (x+300)*600+(y+300)
+def fill_slow(x0,y0):
+	points=[0]*(600*600)
+	st=[(x0,y0)]
+	def fill():
+		x,y=st.pop()
+		if points[idx(x,y)]: return
+		if not test(x,y): return
+		points[idx(x,y)]=1
+		st.append((x+1,y))
+		st.append((x,y+1))
+		st.append((x-1,y))
+		st.append((x,y-1))
+	while len(st) > 0:
+		fill()
+	return sum(points)
+print(fill_slow(0,0))
