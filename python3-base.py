e0ffef9d3fee8e9a0df1ec835b1548f8ec69527f
Ivan Filonov <ivan.filonov@gmail.com>
2015-02-15 15:49:50 +0300

    wip

---
+++ python3-base.py
@@ -0,0 +1,17 @@
+#"""Sample code to read in test cases:
+
+import sys
+	
+def process(s):
+    pass
+
+test_cases = open(sys.argv[1], 'r')
+for test in test_cases:
+    # ignore test if it is an empty line
+    # 'test' represents the test case, do something with it
+    test=test.strip()
+    if len(test)==0:
+        continue
+    process(test)
+
+test_cases.close()
