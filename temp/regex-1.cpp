5fb0a0cfb64ddb09b5e22cf840268a3507fe52f0
Ivan Filonov <ivan.filonov@gmail.com>
2015-02-24 14:47:34 +0300

    wip

--- temp/regex-1.cpp
+++ temp/regex-1.cpp
@@ -1,8 +1,16 @@
 #include <iostream>
+
+//#define CPP_REGEX
+
+#ifdef CPP_REGEX
 #include <regex>
+#else//#ifdef CPP_REGEX
+#include <regex.h>
+#endif//#ifdef CPP_REGEX
 
 int main() {
   std::string ss[] = { "a.a", "abc.a", "abc.a.b" , "b.b"  };
+#ifdef CPP_REGEX
   std::regex r1 (".*\\.a");
   for(auto & s : ss) {
     std::cout << s << "->" << std::regex_match(s, r1) << "\n";
@@ -12,6 +20,21 @@ int main() {
   for(auto & s : ss) {
     std::cout << s << "->" << std::regex_match(s, r1) << "\n";
   }
+#else//#ifdef CPP_REGEX
+  regex_t rx;
+  const std::string rxs { R"(.*\.a)" };
+  if(regcomp(&rx, rxs.c_str(), REG_EXTENDED)) {
+    std::cout << "bad regex: " << rxs << "\n";
+    return 1;
+  }
+
+  for(auto &s : ss) {
+    regmatch_t m;
+    if(!regexec(&rx, s.c_str(), 1, &m, 0)) {
+      std::cout << s << "->" << s.substr(m.rm_so, m.rm_eo-m.rm_so) << "\n";
+    }
+  }
+#endif//#ifdef CPP_REGEX
   return 0;
 }
 
