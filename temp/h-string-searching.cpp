c82e340c7774156ce0eaf2e6b940776439ffb996
Ivan Filonov <ivan.filonov@gmail.com>
2015-03-06 23:17:27 +0300

    wip Fri Mar  6 23:17:27 MSK 2015

--- temp/h-string-searching.cpp
+++ temp/h-string-searching.cpp
@@ -2,8 +2,6 @@
 #include <iostream>
 #include <vector>
 
-#define TEST
-
 namespace {
 #ifdef TEST
   void test();
@@ -53,48 +51,76 @@ namespace {
     }
   }
 
-  size_t lookup_impl(const std::string& where, const std::string &s) {
+  size_t lookup_impl(const std::string& where, const size_t from, const std::string &s) {
+    // brute force approach first
+    if(from + s.length() > where.length()) {
+      return std::string::npos;
+    }
+    for(size_t i = from, j = where.length() - s.length() + 1; i < j; ++i) {
+      bool flag = true;
+      for(size_t pos = 0; pos < s.length() && flag; ++pos) {
+        flag &= s[pos] == where[i + pos];
+      }
+      if(flag) {
+        return i;
+      }
+    }
     return std::string::npos;
+  }
+
+  bool rlookup(const std::string& where, const std::vector<std::string> &v, const size_t i) {
+    if(v.size() <= i) {
+      return true;
+    }
+    const auto &s = v[i];
+    for(size_t from = lookup_impl(where, 0, s); std::string::npos != from; from = lookup_impl(where, from + 1, s)) {
+      if( from + s.length() > where.length()) {
+        break;
+      }
+      if(rlookup(where.substr(from+s.length()), v, i + 1)) {
+        return true;
+      }
+    }
+    return false;
   }
 
   void process(std::string line) {
 #ifdef TEST
     std::cout << "s = '" << line << "'\n";
 #endif //#ifdef TEST
-  //    std::istringstream ss { line };
     const int comma_pos = line.find(',');
     std::string str = line.substr(0, comma_pos);
     std::string pattern = line.substr(comma_pos + 1);
-    std::cout << "str = '" << str << "', pattern = '" << pattern << "'\n";
 
     std::vector<std::string> parts;
     line = pattern;
     while(!line.empty()) {
       bool flag = false;
+      bool escape = false;
       for(size_t i = 0; i != line.length(); ++i) {
-        if('*' == line[i] && (!i || '\\' != line[i-1])) {
+        const auto c = line[i];
+        if('*' == c && !escape) {
           parts.push_back(line.substr(0,i));
-          std::cout << "pushing '" << parts.back() << "'\n";
-          line = line.substr(i+1);
+          if(parts.back().empty()) {
+            parts.pop_back();
+          }
+          line.erase(0, i + 1);
           flag = true;
           break;
         }
+        escape = '\\' == c;
       }
       if(!flag) {
         parts.emplace_back(std::move(line));
       }
     }
-    std::cout << "parts: ";
-    bool mid = false;
-    for(auto &s : parts) {
-      if(mid) {
-        std::cout << ", ";
-      } else {
-        mid = true;
-      }
-      std::cout << s;
-    }
-    std::cout << "\n";
 
+    for(auto & s : parts) {
+      for(auto p = lookup_impl(s, 0, "\\*"); std::string::npos != p; p = lookup_impl(s, p, "\\*")) {
+        s.erase(p, 1);
+      }
+    }
+    
+    std::cout << (rlookup(str, parts, 0) ? "true" : "false")  << "\n";
   }
 }
