4c8a0a0f47f34d2479c537b9c7e54e1aa437060f
Ivan Filonov <ivan.filonov@gmail.com>
2015-03-06 21:52:32 +0300

    wip @Fri Mar  6 21:52:32 MSK 2015

---
+++ temp/h-glue-shredded-pieces.cpp
@@ -0,0 +1,54 @@
+#include <fstream>
+#include <iostream>
+#include <vector>
+
+#define TEST
+
+namespace {
+#ifdef TEST
+  void test();
+#endif //#ifdef TEST
+  void process_file(char*);
+}
+
+int main(int argc, char ** argv) {
+#ifdef TEST
+  test();
+#else //#ifdef TEST
+  process_file(argv[1]);
+#endif //#ifdef TEST
+	return 0;
+}
+
+namespace {
+  void process(std::string s);
+
+#ifdef TEST
+  void test() {
+    std::vector<std::string> v_test {
+"|deEva|lan t|to ha|evil |ankin|il-ev|o hac| to h|vil p|an to|The e|CodeE| evil|plan |hack |Eval |ack C|l ran|king.|l-evi|evil-|-evil|l pla|il pl| hack|al ra|vil-e|odeEv|he ev|n to |ck Co|eEval|nking| rank| Code|e evi|ranki|k Cod| plan|val r|"
+    };
+    std::vector<std::string> v_expect {
+"The evil-evil plan to hack CodeEval ranking."
+    };
+    for(int i = 0, j = std::min(v_test.size(), v_expect.size()); i < j; ++i) {
+      process(v_test[i]);
+      std::cout << v_expect[i] << "\n";
+    }
+  }
+#endif //#ifdef TEST
+
+  void process_file(char* path) {
+    std::ifstream stream(path);
+    for(std::string line; std::getline(stream, line); ) {
+      process(line);
+    }
+  }
+
+  void process(std::string line) {
+#ifdef TEST
+    std::cout << "s = '" << line << "'\n";
+#endif //#ifdef TEST
+  //    std::istringstream ss { line };
+  }
+}
