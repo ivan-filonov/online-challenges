1a6550af26cbea55e2f70ac35ffee3f1688bb135
Ivan Filonov <ivan.filonov@gmail.com>
2015-04-12 13:32:34 +0300

    wip Sun Apr 12 13:32:34 MSK 2015

--- temp/m-2048.cpp
+++ temp/m-2048.cpp
@@ -2,8 +2,6 @@
 #include <iostream>
 #include <vector>
 
-#define TEST
-
 namespace {
 #ifdef TEST
   void test();
@@ -21,20 +19,45 @@ int main(int argc, char ** argv) {
 }
 
 namespace {
+  using std::string;
+  using std::vector;
+
+  vector<string> v_dir {"UP", "DOWN", "RIGHT", "LEFT"};
   void process(std::string line) {
 #ifdef TEST
     std::cout << "s = '" << line << "'\n";
 #endif //#ifdef TEST
-  //    std::istringstream ss { line };
+    auto sc = line.find(';');
+    auto sc2 = line.find(';', sc + 1);
+    auto dir = line.substr(0, sc);
+    auto dim = std::stoi(line.substr(sc + 1, sc2 - sc - 1));
+    vector<int> board (dim * dim);
+    auto pos = sc2 + 1;
+    for(auto & dst : board) {
+      size_t ofs;
+      dst = std::stoi(line.substr(pos), &ofs);
+      pos += ofs + 1;
+    }
+    // TODO: process
+    //...
+    for(int i = 0; i < dim * dim; ++i) {
+      if(i) {
+        std::cout << (i%dim ? ' ' :'|');
+      }
+      std::cout << board[i];
+    }
+    std::cout << "\n";
   }
 
 #ifdef TEST
   void test() {
     std::vector<std::string> v_test {
-
+      "RIGHT; 4; 4 0 2 0|0 0 0 8|4 0 2 4|2 4 2 2",
+      "UP; 4; 2 0 2 0|0 2 0 4|2 8 0 8|0 8 0 16",
     };
     std::vector<std::string> v_expect {
-
+      "0 0 4 2|0 0 0 8|0 4 2 4|0 2 4 4",
+      "4 2 2 4|0 16 0 8|0 0 0 16|0 0 0 0",
     };
     for(int i = 0, j = std::min(v_test.size(), v_expect.size()); i < j; ++i) {
       process(v_test[i]);
