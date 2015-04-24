d9cd7dbbbadb563151b8afe1c8442985378daf75
Ivan Filonov <ivan.filonov@gmail.com>
2015-04-24 19:13:00 +0300

    wip Fri Apr 24 19:13:00 MSK 2015

---
+++ cpp-bulk.cpp
@@ -0,0 +1,56 @@
+#include <fstream>
+#include <iostream>
+#include <vector>
+
+#ifdef TEST
+void test();
+#endif //#ifdef TEST
+void process_file(char*);
+
+int main(int argc, char ** argv) {
+#ifdef TEST
+  test();
+#else //#ifdef TEST
+  process_file(argv[1]);
+#endif //#ifdef TEST
+	return 0;
+}
+
+using std::string;
+template<typename V> using vector = std::vector<V>;
+
+void add_line(string line) {
+#ifdef TEST
+  std::cout << "s = '" << line << "'\n";
+#endif //#ifdef TEST
+//    std::istringstream ss { line };
+}
+
+void run() {
+}
+
+#ifdef TEST
+void test() {
+  vector<string> v_test {
+
+  };
+  for(auto & t : v_test) {
+    add_line(t);
+  }
+  run();
+  vector<string> v_expect {
+
+  };
+  for(auto & e : v_expect) {
+    std::cout << e << "\n";
+  }
+}
+#endif //#ifdef TEST
+
+void process_file(char* path) {
+  std::ifstream stream(path);
+  for(string line; std::getline(stream, line); ) {
+    add_line(line);
+  }
+  run();
+}
