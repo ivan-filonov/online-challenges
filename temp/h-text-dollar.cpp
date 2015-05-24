142541f24bfb6e141e834350961646a5fd1375b9
Ivan Filonov <ivan.filonov@gmail.com>
2015-05-25 00:43:32 +0300

    wip Mon May 25 00:43:32 MSK 2015

--- temp/h-text-dollar.cpp
+++ temp/h-text-dollar.cpp
@@ -19,11 +19,80 @@ int main(int argc, char ** argv) {
 using std::string;
 template<typename V> using vector = std::vector<V>;
 
+const vector<string> numbers {
+  "",
+  "One",
+  "Two",
+  "Three",
+  "Four",
+  "Five",
+  "Six",
+  "Seven",
+  "Eight",
+  "Nine",
+  "Ten",
+  "Eleven",
+  "Twelve",
+  "Thirteen",
+  "Fourteen",
+  "Fifteen",
+  "Sixteen",
+  "Seventeen",
+  "Eighteen",
+  "Nineteen"
+};
+
+const vector<string> n10 {
+  "Twenty",
+  "Thirty",
+  "Forty",
+  "Fifty",
+  "Sixty",
+  "Seventy",
+  "Eighty",
+  "Ninety",
+};
+
+void sub1k(int v) {
+  if(v >= 100) {
+    std::cout << numbers[v/100] << "Hundred";
+    v %= 100;
+  }
+  if(v < 20) {
+    std::cout << numbers[v];
+  } else {
+    std::cout << n10[(v - 20)/10];
+    v %= 10;
+    if(v) {
+      std::cout << numbers[v];
+    }
+  }
+}
+
 void process(string line) {
 #ifdef TEST
   std::cout << "s = '" << line << "'\n";
 #endif //#ifdef TEST
 //    std::istringstream ss { line };
+  auto v = std::stol(line);
+  const auto c_1e6 = 1000000;
+  const auto c_1e3 = 1000;
+  if(v) {
+    if(v >= c_1e6) {
+      sub1k(v / c_1e6);
+      std::cout << "Million";
+      v %= c_1e6;
+    }
+    if(v >= c_1e3) {
+      sub1k(v / c_1e3);
+      std::cout << "Thousand";
+      v %= c_1e3;
+    }
+    sub1k(v);
+  } else {
+    std::cout << "Zero";
+  }
+  std::cout << "Dollars\n";
 }
 
 #ifdef TEST
