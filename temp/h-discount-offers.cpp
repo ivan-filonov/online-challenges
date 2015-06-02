9f62e35bfb52b01b43c61af3baf846ab4340c1fe
Ivan Filonov <ivan.filonov@gmail.com>
2015-06-03 00:33:04 +0300

    wip Wed Jun  3 00:33:04 MSK 2015

--- temp/h-discount-offers.cpp
+++ temp/h-discount-offers.cpp
@@ -95,14 +95,9 @@ double score(const cust_t & cust, const prod_t & prod) {
 }
 
 void process(string line) {
-  grow_primes(100);
-#ifdef TEST
-  std::cout << "s = '" << line << "'\n";
-#endif //#ifdef TEST
-
   vector<string> vs;
-  vector<cust_t> vc;//customers
-  vector<prod_t> vp;//products
+  vector<cust_t> v_customers;//customers
+  vector<prod_t> v_products;//products
 
   // prepare lists of customers and products
   bool part1 = true;
@@ -113,13 +108,13 @@ void process(string line) {
     vs.emplace_back(line.substr(pos, delim - pos));
 
     if(part1) {
-      vc.emplace_back();
-      vc.back().sid = sid;
-      vc.back().set(vs.back());
+      v_customers.emplace_back();
+      v_customers.back().sid = sid;
+      v_customers.back().set(vs.back());
     } else {
-      vp.emplace_back();
-      vp.back().sid = sid;
-      vp.back().set(vs.back());
+      v_products.emplace_back();
+      v_products.back().sid = sid;
+      v_products.back().set(vs.back());
     }
     
     if(delim == string::npos) {
@@ -131,51 +126,113 @@ void process(string line) {
     pos = delim + 1;
   }
 
-  // precalculate pairs
-  vector<double> prec (vc.size() * vp.size());
-  auto pwid = vp.size();
-  auto phei = vc.size();
-  for(int ic = 0; ic != phei; ++ic) {
-    for(int ip = 0; ip != pwid; ++ip) {
-      prec[ic * pwid + ip] = score(vc[ic], vp[ip]);
+  // weight matrix with fake line and column at index 0
+  vector<double> prec ((1 + v_customers.size()) * (1 + v_products.size()));
+  // prec[customer * pwid + product] - customer and product are now 1-based
+  auto pwid = v_products.size() + 1 ;
+  auto phei = v_customers.size() + 1;
+  for(int customer = 0; customer < v_customers.size(); ++customer) {
+    for(int product = 0; product < v_products.size(); ++product) {
+      prec[customer * pwid + pwid + product + 1] = -score(v_customers[customer], v_products[product]);
     }
   }
-  // idx = col + line * pwid
-
-  if(phei > pwid) {//transpose it
-    vector<double> temp (prec.size());
-    for(int i = 0; i != phei; ++i) {
-      for(int j = 0; j != pwid; ++j) {
-        temp[i + j * phei] =  prec[i * pwid + j];
+  
+  if(v_customers.size() > v_products.size()) {
+    vector<double> temp(prec.size());
+    for(int line = 0; line < phei; ++line) {
+      for(int col = 0; col < pwid; ++col) {
+        temp[line + phei * col] = prec[line * pwid + col];
       }
     }
-    std::swap(phei, pwid);
+    std::swap(pwid, phei);
     temp.swap(prec);
   }
 
-  vector<double> vsum(pwid, 0.0);
+  // idx = col + line * pwid
 
-  for(int line = 0; line < phei; ++line) {
-    vector<double> nvsum(pwid, 0.0);
-    ;
-    nvsum.swap(vsum);
+  // hungarian alg now:
+  const auto num_lines = phei - 1;
+  const auto num_columns = pwid - 1;
+
+  vector<double> line_level(phei, 0.0);
+  vector<double> column_level(pwid, 0.0);
+  vector<int> assigned_line_for_column(pwid, 0);
+  vector<int> backtrack(pwid, 0);
+  
+  vector<double> minv(pwid);
+  vector<bool> used(pwid);
+
+  for(auto line_to_add = 1; line_to_add <= num_lines; ++line_to_add) {
+    assigned_line_for_column[0] = line_to_add;
+    int cur_column = 0;
+
+    minv.assign(pwid, 1e9);
+    used.assign(pwid, false);
+
+    do {
+      used[cur_column] = true;
+      const int cur_line = assigned_line_for_column[cur_column];
+      double delta = 1e9;
+      int next_column = 0;
+
+      for(auto column = 1; column <= num_columns; ++column) {
+        if(used[column]) {
+          continue;
+        }
+
+        const auto cur = prec[cur_line * pwid + column] - line_level[cur_line] - column_level[column];
+        if(cur < minv[column]) {
+          minv[column] = cur;
+          backtrack[column] = cur_column;
+        }
+
+        if(minv[column] < delta) {
+          delta = minv[column];
+          next_column = column;
+        }
+      }
+
+      for(auto column = 0; column <= num_columns; ++column) {
+        if(used[column]) {
+          line_level[assigned_line_for_column[column]] += delta;
+          column_level[column] -= delta;
+        } else {
+          minv[column] -= delta;
+        }
+      }
+
+      cur_column = next_column;
+    } while(0 != assigned_line_for_column[cur_column]);
+    do {
+      const int prev_column = backtrack[cur_column];
+      assigned_line_for_column[cur_column] = assigned_line_for_column[prev_column];
+      cur_column = prev_column;
+    } while(0 != cur_column);
   }
 
   // print result
-  double res = 0.25;
-  std::cout.precision(2);
-  std::cout.setf(std::cout.fixed);
-  std::cout << res << "\n";
+  double res = 0.0;
+  for(int column = 1; column <= num_columns; ++column) {
+    auto line = assigned_line_for_column[column];
+    res += prec[line * pwid + column];
+  }
+//  std::cout.precision(2);
+//  std::cout.setf(std::cout.fixed);
+  std::cout << std::abs(res) << "\n";
 }
 
 #ifdef TEST
 void test() {
   vector<string> v_test {
+    "Jack Abraham;iPad 2 - 4-pack,Girl Scouts Thin Mints,Nerf Crossbow",
+    "Jack Abraham,John Evans,Ted Dziuba;iPad 2 - 4-pack",
     "Jack Abraham,John Evans,Ted Dziuba;iPad 2 - 4-pack,Girl Scouts Thin Mints,Nerf Crossbow",
     "Jeffery Lebowski,Walter Sobchak,Theodore Donald Kerabatsos,Peter Gibbons,Michael Bolton,Samir Nagheenanajar;Half & Half,Colt M1911A1,16lb bowling ball,Red Swingline Stapler,Printer paper,Vibe Magazine Subscriptions - 40 pack",
     "Jareau Wade,Rob Eroh,Mahmoud Abdelkader,Wenyi Cai,Justin Van Winkle,Gabriel Sinkin,Aaron Adelson;Batman No. 1,Football - Official Size,Bass Amplifying Headphones,Elephant food - 1024 lbs,Three Wolf One Moon T-shirt,Dom Perignon 2000 Vintage",
   };
   vector<string> v_expect {
+    "7.00",
+    "6.00",
     "21.00",
     "83.50",
     "71.25",
