326a2bfb3ae243ff97e5e0116f8a7142910207db
Ivan Filonov <ivan.filonov@gmail.com>
2015-06-17 03:58:22 +0300

    wip Wed Jun 17 03:58:22 MSK 2015

--- codeeval/temp/h-poker-hands.cpp
+++ codeeval/temp/h-poker-hands.cpp
@@ -25,7 +25,7 @@ template<typename V> using vector = std::vector<V>;
 string ranks { "23456789TJQKA" };
 string suites{ "DHCS" };
 
-bool  sort_by (const string & c1, const string & c2) {
+bool less_by_rank (const string & c1, const string & c2) {
   if(c1[0] == c2[0]) { // same rank
     return c2[1] < c1[1];
   }
@@ -46,133 +46,57 @@ enum class hr {
 };
 
 struct hand_t {
-  int m[13][4];
-  int per_suite_counts[4];
-  int per_rank_counts[13];
-  int chain_prefix[13];
-  int num_chains[5];
-  hr  hand_rank;
-  int straight_rank;
-  int pair1_rank;
-  int pair2_rank;
-  int triple_rank;
-  int four_rank;
-
-  void calc_hand_rank() {
-    hand_rank = hr::highest_card;
-    for(int suite = 0; suite < 4; ++suite) {
-      if(per_suite_counts[suite] == 5) {
-        hand_rank = hr::flush;
-      }
-    }
-    if(num_chains[4]) {
-      for(int rank = 4; rank < 13; ++rank) {
-        if(5 == chain_prefix[rank]) {
-          straight_rank = rank;
-          break;
-        }
-      }
-      if(hand_rank == hr::flush) {
-        hand_rank = hr::straight_flush;
-        if(per_rank_counts[12]) {
-          hand_rank = hr::royal_flush;
-        }
-      } else {
-        hand_rank = hr::straight;
-      }
-    }
-    if(hr::highest_card == hand_rank) {
-      int pairs = 0;
-      int triples = 0;
-      int fours = 0;
-      for(int rank = 0; rank < 13; ++rank) {
-        switch(per_rank_counts[rank]) {
-          case 2: 
-            if(pair1_rank) {
-              pair2_rank = rank;
-              if(pair2_rank > pair1_rank) {
-                std::swap(pair1_rank, pair2_rank);
-              }
-            } else {
-              pair1_rank = rank;
-            }
-            ++pairs; 
-            break;
-          case 3: 
-            triple_rank = rank;
-            ++triples; 
-            break;
-          case 4: 
-            four_rank = rank;
-            ++fours;
-            break;            
-        }
-      }
-      if(fours) {
-        hand_rank = hr::four_of_a_kind;
-      }
-      if(triples) {
-        hand_rank = hr::three_of_a_kind;
-        if(pairs) {
-          hand_rank = hr::full_house;
-        }
-      }
-      if(pairs) {
-        hand_rank = hr::one_pair;
-        if(pairs > 1) {
-          hand_rank = hr::two_pairs;
-        }
-      }
-    }
-  }
-
-  int compare_by_highest(const hand_t & other) const {
-    for(int rank = 12; rank >= 0; --rank) {
-      if(per_rank_counts[rank] != other.per_rank_counts[rank]) {
-        return per_rank_counts[rank] - other.per_rank_counts[rank];
-      }
-    }
-    return 0;
-  }
-
-  hand_t() : m { 0, }, 
-             per_suite_counts { 0, }, 
-             per_rank_counts { 0, }, 
-             chain_prefix { 0, }, 
-             num_chains { 0, },
-             straight_rank (0),
-             pair1_rank (0),
-             pair2_rank (0),
-             triple_rank (0),
-             four_rank (0)
-             { }
+  vector<string> initial;
+  vector<string> same_1;
+  vector<string> same_2;
+  bool is_flush;
+  bool is_straight;
 
   void put(string && card) {
-    int rank = ranks.find(card[0]);
-    int suite = suites.find(card[1]);
-    m[rank][suite] = 1;
+    initial.emplace_back(std::move(card));
   }
 
-  void precalc() {
-    for(int rank = 0; rank < 13; ++rank) {
-      for(int suite = 0; suite < 4; ++suite) {
-        per_suite_counts[suite] += m[rank][suite];
-        per_rank_counts[rank] += m[rank][suite];
-      }
-      if(per_rank_counts[rank]) {
-        chain_prefix[rank] = 1;
-        if(rank && per_rank_counts[rank-1]) {
-          chain_prefix[rank] = chain_prefix[rank-1] + 1;
-          chain_prefix[rank-1] = 0;
-        }
-      }
-    }
-    for(int rank = 0; rank < 13; ++rank) {
-      if(chain_prefix[rank]) {
-        num_chains[chain_prefix[rank]-1]++;
+  void calc() {
+    // 1. sort by suite:
+    std::sort(initial.begin(), initial.end(), [](const string & c1, const string & c2) { return c1[1] < c2[1]; });
+    int cur_suite = 0;
+    int suite_cnt = 0;
+    int suite_max = 0;
+    for(auto & card : initial) {
+      if(card[1] == cur_suite) {
+        ++suite_cnt;
+        suite_max = std::max(suite_max, suite_cnt);
+      } else {
+        cur_suite = card[1];
+        suite_cnt = 1;
       }
     }
-    calc_hand_rank();
+    is_flush = 5 == suite_max;
+    // 2. sort by rank descending:
+    std::sort(initial.begin(), initial.end(), less_by_rank);
+    for(auto & card : initial) {
+      card += (char)('a' + ranks.find(card[0]));
+    }
+    // check for is_straight:
+    is_straight = true;
+    for(int i = 1; (i != initial.size()) && is_straight; ++i) {
+      is_straight = 1 == ranks.find(initial[i-1][0]) - ranks.find(initial[i][0]);
+    }
+    // check for groups:
+    int group_rank = -1;
+    int group_size = 0;
+    for(auto & card : initial) {
+      if(card[2] - 'a' == group_rank) {
+        ++group_size;
+      } else {
+        group_rank = card[2] - 'a';
+        group_size = 1;
+        //TODO: grow group
+      }
+
+      }
+    }
+
   }
 };
 
@@ -181,64 +105,22 @@ void process(string line) {
   std::cout << "s = '" << line << "'\n";
 #endif //#ifdef TEST
   std::istringstream ss { line };
+  
   hand_t left;
-  hand_t right;
   for(int i = 0; i < 5; ++i) {
     string card;
     ss >> card;
     left.put(std::move(card));
   }
+  left.calc();
+
+  hand_t right;
   for(int i = 0; i < 5; ++i) {
     string card;
     ss >> card;
     right.put(std::move(card));
   }
-  left.precalc();
-  right.precalc();
-  if(left.hand_rank > right.hand_rank) {
-    std::cout << "left\n";
-  } else if(left.hand_rank < right.hand_rank) {
-    std::cout << "right\n";
-  } else {
-    int comp = 0;
-    switch(left.hand_rank) {
-      case hr::one_pair:
-        comp = left.pair1_rank - right.pair1_rank;
-        break;
-      case hr::two_pairs:
-        comp = left.pair1_rank - right.pair1_rank;
-        if(!comp) {
-          comp = left.pair2_rank - right.pair2_rank;
-        }
-        break;
-      case hr::three_of_a_kind:
-        comp = left.triple_rank - right.triple_rank;
-        break;
-      case hr::full_house:
-        comp = std::max(left.triple_rank, left.pair1_rank) - std::max(right.triple_rank, right.pair1_rank);
-        if(!comp) {
-          comp = std::min(left.triple_rank, left.pair1_rank) - std::min(right.triple_rank, right.pair1_rank);
-        }
-        break;
-      case hr::four_of_a_kind:
-        comp = left.four_rank - right.four_rank;
-        break;
-      case hr::straight:
-      case hr::straight_flush:
-        comp = left.straight_rank - right.straight_rank;
-        break;
-    };
-    if(!comp) {
-      comp = left.compare_by_highest(right);
-    }
-    if(comp < 0) {
-      std::cout << "right\n";
-    } else if(!comp) {
-      std::cout << "none\n";
-    } else {
-      std::cout << "left\n";
-    }
-  }
+  right.calc();
 }
 
 #ifdef TEST
