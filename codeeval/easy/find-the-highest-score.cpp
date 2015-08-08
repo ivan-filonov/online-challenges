e23b995a1a6c473ea80a6b079e9a2b230a05073d
Ivan Filonov <ivan.filonov@gmail.com>
2015-08-08 15:48:35 +0300

    wip Sat Aug  8 15:48:35 MSK 2015

--- codeeval/easy/find-the-highest-score.cpp
+++ codeeval/easy/find-the-highest-score.cpp
@@ -1,5 +1,6 @@
 #include <fstream>
 #include <iostream>
+#include <sstream>
 #include <vector>
 
 #ifdef TEST
@@ -22,18 +23,47 @@ template<typename V> using vector = std::vector<V>;
 
 void process(string line) {
 #ifdef TEST
-  std::cout << "s = '" << line << "'\n";
+	std::cout << "s = '" << line << "'\n";
 #endif //#ifdef TEST
-//    std::istringstream ss { line };
+	std::istringstream ss { line };
+
+	int pos = 0;
+	vector<int> v;
+
+	for(string s; ss >> s;) {
+		if(s == "|") {
+			pos = 0;
+			continue;
+		}
+		auto i = std::stoi(s);
+		if(v.size() == pos) {
+			v.resize(pos + 1);
+			v[pos] = i;
+		} else {
+			v[pos] = std::max(i, v[pos]);
+		}
+		++pos;
+	}
+
+	bool mid = false;
+	for(int i : v) {
+		std::cout << (mid ? " " : "") << i;
+		mid = true;
+	}
+	std::cout << "\n";
 }
 
 #ifdef TEST
 void test() {
   vector<string> v_test {
-
+	"72 64 150 | 100 18 33 | 13 250 -6",
+	"10 25 -30 44 | 5 16 70 8 | 13 1 31 12",
+	"100 6 300 20 10 | 5 200 6 9 500 | 1 10 3 400 143",
   };
   vector<string> v_expect {
-
+	"100 250 150",
+	"13 25 70 44",
+	"100 200 300 400 500",
   };
   for(int i = 0, j = std::min(v_test.size(), v_expect.size()); i < j; ++i) {
     process(v_test[i]);
