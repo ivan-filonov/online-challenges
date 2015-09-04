47fd33251ef45dabd132dd26f3bb60d07343929a
Ivan Filonov <ivan.filonov@gmail.com>
2015-09-04 19:26:07 +0300

    wip Fri Sep  4 19:26:06 MSK 2015

---
+++ sync11.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+if [ "$HOSTNAME" = vbox-gentoo ]; then
+	rsync -av --delete-after /home/user/projects/online-challenges/ rsync://192.168.75.11/projects/online-challenges
+#elif [ "$HOSTNAME" = vbox-gentoo-2 ]; then
+#	rsync -av --delete-after rsync://192.168.75.10/projects/online-challenges /home/user/projects/online-challenges/
+fi
