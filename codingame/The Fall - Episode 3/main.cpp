db66c6adb97a05a06b7a6a516e42f2c6f9868fd8
Ivan Filonov <ivan.filonov@gmail.com>
2022-11-13 02:47:17 +0300

    The Fall 3 wip

--- codingame/The Fall - Episode 3/main.cpp
+++ codingame/The Fall - Episode 3/main.cpp
@@ -20,7 +20,8 @@ static constexpr int pos_top = 0;
 static constexpr int pos_left = 1;
 static constexpr int pos_right = 2;
 
-static int convert_pos (const std::string& s);
+static int                convert_pos (const std::string& s);
+static const std::string& convert_pos (int s);
 
 static constexpr int bit_h = 1;
 static constexpr int bit_v = 2;
@@ -39,6 +40,14 @@ static constexpr int movement_masks[14] = {0, bit_l2b | bit_v | bit_r2b, bit_h,
 
 static const std::string& test_input ();
 
+struct Cmd {
+  int rotation;
+  int x;
+  int y;
+
+  std::string to_string ();
+};
+
 struct Board {
   int width;
   int height;
@@ -47,6 +56,8 @@ struct Board {
   std::array<int8_t, 20 * 20> board;
 
   void read_from (std::istream& in);
+  bool step (int x, int y, int pos, int* out_x, int* out_y, int* out_pos);
+  void apply (const Cmd& cmd);
 };
 
 struct State {
@@ -58,6 +69,9 @@ struct State {
 
   void read_from (std::istream& in);
 };
+
+enum class SimResult { FORK, WIN, FAIL };
+std::string to_string (SimResult sr);
 
 struct Solver {
   Board board;
@@ -85,10 +99,12 @@ int main ()
 
   Board board;
   board.read_from (in);
+  const auto max_step = 4; //(board.width + 1) * board.height
+
   Solver solver;
   solver.init (board);
 
-  for (int step = 0; step < (board.width + 1) * board.height; ++step) {
+  for (int step = 0; step < max_step; ++step) {
     State state;
     state.read_from (in);
     out << solver.run (state) << std::endl;
@@ -113,6 +129,13 @@ static const std::string test_input_string = R"(14 8
 static const std::string& test_input ()
 {
   return test_input_string;
+}
+
+static const std::array<std::string, 3> pos_names = {"TOP", "LEFT", "RIGHT"};
+
+static const std::string& convert_pos (int pos)
+{
+  return pos_names.at (pos);
 }
 
 static int convert_pos (const std::string& s)
@@ -178,4 +201,103 @@ void State::read_from (std::istream& in)
 void Solver::init (const Board& initial_board)
 {
   board = initial_board;
+}
+
+bool Board::step (int x, int y, int pos, int* out_x, int* out_y, int* out_pos)
+{
+  const auto index = y * 20 + x;
+  const auto cell_value = board[index];
+  const auto cell_bits = movement_masks[std::abs (cell_value)];
+  switch (pos) {
+  case pos_top:
+    if (cell_bits & bit_v) {
+      *out_pos = pos_top;
+    } else if (cell_bits & bit_t2l) {
+      *out_pos = pos_right;
+    } else if (cell_bits & bit_t2r) {
+      *out_pos = pos_left;
+    } else {
+      return false;
+    }
+    break;
+  case pos_left:
+    if (cell_bits & bit_h) {
+      *out_pos = pos_left;
+    } else if (cell_bits & bit_l2b) {
+      *out_pos = pos_top;
+    } else {
+      return false;
+    }
+    break;
+  case pos_right:
+    if (cell_bits & bit_h) {
+      *out_pos = pos_right;
+    } else if (cell_bits & bit_r2b) {
+      *out_pos = pos_top;
+    } else {
+      return false;
+    }
+    break;
+  default: return false;
+  }
+  if (*out_pos == pos_top) {
+    *out_x = x;
+    *out_y = y + 1;
+    return *out_y < height || *out_x == exit_x;
+  } else if (*out_pos == pos_left) {
+    *out_x = x + 1;
+    *out_y = y;
+    return *out_x < width;
+  } else if (*out_pos == pos_right) {
+    *out_x = x - 1;
+    *out_y = y;
+    return *out_x >= 0;
+  }
+  return false;
+}
+
+void Board::apply (const Cmd& cmd)
+{
+  if (cmd.rotation == 0) {
+    return;
+  }
+  const int index = cmd.y * 20 + cmd.x;
+
+  auto& ref = board[index];
+  if (ref < 2 || ref > 13) {
+    throw std::runtime_error ("invalid cell rotation");
+  }
+  if (cmd.rotation == -1) {
+    ref = left_rotation[ref];
+    return;
+  }
+  if (cmd.rotation == 1) {
+    ref = right_rotation[ref];
+    return;
+  }
+  throw std::runtime_error ("malformed command");
+}
+
+std::string Cmd::to_string ()
+{
+  if (rotation == 0) {
+    return "WAIT";
+  }
+  std::stringstream ss;
+  ss << x << " " << y;
+  if (rotation < 0) {
+    ss << " LEFT";
+  } else {
+    ss << " RIGHT";
+  }
+  return ss.str ();
+}
+
+std::string to_string (SimResult sr)
+{
+  switch (sr) {
+  case SimResult::FORK: return "FORK";
+  case SimResult::FAIL: return "FAIL";
+  case SimResult::WIN: return "WIN";
+  }
 }
